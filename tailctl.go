cb8f1636be26d6d4bb73888df5a66677722541a8
David Baynard <git@baynard.dev>
2024-10-23 16:18:30 +0100

    Write policyfile to stdout

--- tailctl.go
+++ tailctl.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"context"
+	"log"
 	"os"
 
 	tsclient "github.com/tailscale/tailscale-client-go/v2"
 )
 
 func main() {
+	logger := log.New(os.Stderr, "", 0)
+
 	client := &tsclient.Client{
 		Tailnet: os.Getenv("TAILSCALE_TAILNET"),
 		HTTP: tsclient.OAuthConfig{
 			ClientID:     os.Getenv("TAILSCALE_OAUTH_CLIENT_ID"),
 			ClientSecret: os.Getenv("TAILSCALE_OAUTH_CLIENT_SECRET"),
-			Scopes:       []string{"all:write"},
+			Scopes:       []string{"acl", "devices:read"},
 		}.HTTPClient(),
 	}
 
-	devices, err := client.Devices().List(context.Background())
+	policyFile, err := client.PolicyFile().Raw(context.Background())
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	os.Stdout.WriteString(policyFile)
 }
